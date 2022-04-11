2d8e32c360eeeaccc91dc2ea2a50e4e95effa794
Vcslav <vcslav.v@gmail.com>
2022-04-11 18:53:46 +0300

    test

--- pb_finam/scheluder.py
+++ pb_finam/scheluder.py
@@ -7,7 +7,7 @@ sched = BlockingScheduler()
 
 
 @logger.catch
-@sched.scheduled_job('cron', hour=15, minute=51)
+@sched.scheduled_job('cron', hour=15, minute=55)
 def add_stripe_transactions():
     logger.info('Add stripe transactions')
     next_last_payment, payments_data = stripe_connector.get_stripe_transactions()
